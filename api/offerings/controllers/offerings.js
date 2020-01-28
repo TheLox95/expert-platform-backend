8667a51b75f9a00e923dbbe3dc57334c6de876c5
leonardo.b <leonardo.b@solutionbeyond.com>
2020-01-28 17:19:13 -0400

    ADDED code to generate thumbnails for videos

--- api/offerings/controllers/offerings.js
+++ api/offerings/controllers/offerings.js
@@ -4,8 +4,25 @@
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const _ = require('lodash');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
 
 module.exports = {
+  async find(ctx) {
+
+    const offerings = (await strapi.services.offerings.find(ctx.query))
+    .map((o) => {
+      delete o.user.password;
+      return {
+        ...o,
+        videos: o.videos.map(v => ({ ...v, thumbnail: `/uploads/${v.name.split('.')[0]}.png`}))
+      }
+    });
+
+    // Send 200 `ok`
+    ctx.send(offerings);
+},
     following: async (ctx) => {
         const currentUser = await strapi.plugins['users-permissions'].services.user.fetch({ id: 11 })
         const followedUser = await Promise.all(currentUser.following.map(u => {
@@ -35,5 +52,148 @@ module.exports = {
 
 
         ctx.send(nonReadNotifications);
+    },
+    upload: async (ctx) => {
+        const uploadService = strapi.plugins.upload.services.upload;
+
+        // Retrieve provider configuration.
+        const config = await strapi
+          .store({
+            environment: strapi.config.environment,
+            type: 'plugin',
+            name: 'upload',
+          })
+          .get({ key: 'provider' });
+    
+        // Verify if the file upload is enable.
+        if (config.enabled === false) {
+          return ctx.badRequest(
+            null,
+    
+            [
+              {
+                messages: [
+                  {
+                    id: 'Upload.status.disabled',
+                    message: 'File upload is disabled',
+                  },
+                ],
+              },
+            ]
+          );
+        }
+    
+        // Extract optional relational data.
+        const { refId, ref, source, field, path } = ctx.request.body || {};
+        const { files = {} } = ctx.request.files || {};
+    
+        if (_.isEmpty(files)) {
+          return ctx.badRequest(null, [
+            {
+              messages: [{ id: 'Upload.status.empty', message: 'Files are empty' }],
+            },
+          ]);
+        }
+    
+        // Transform stream files to buffer
+        const buffers = await uploadService.bufferize(files);
+    
+        const enhancedFiles = buffers.map(file => {
+          if (file.size > config.sizeLimit) {
+            return ctx.badRequest(null, [
+              {
+                messages: [
+                  {
+                    id: 'Upload.status.sizeLimit',
+                    message: `${file.name} file is bigger than limit size!`,
+                    values: { file: file.name },
+                  },
+                ],
+              },
+            ]);
+          }
+    
+          // Add details to the file to be able to create the relationships.
+          if (refId && ref && field) {
+            Object.assign(file, {
+              related: [
+                {
+                  refId,
+                  ref,
+                  source,
+                  field,
+                },
+              ],
+            });
+          }
+    
+          // Update uploading folder path for the file.
+          if (path) {
+            Object.assign(file, {
+              path,
+            });
+          }
+    
+          return file;
+        });
+    
+        // Something is wrong (size limit)...
+        if (ctx.status === 400) {
+          return;
+        }
+
+        const videoFiles = enhancedFiles.filter(f => f.mime.includes('video'))
+        if (videoFiles.length !== 0) {
+          const thumbnails = videoFiles.map(v => {
+
+            return new Promise((res, rej) => {
+              ffmpeg(`${v.tmpPath}`)
+              .on('end', function(end) {
+                res(end)
+              })
+              .on('error', function(err) {
+                rej(err)
+              })
+              .screenshots({
+                count: 1,
+                folder: `${strapi.config.public.path}/uploads/`,
+                filename: `${v.name.split('.')[0]}`,
+              });
+            });
+          });
+
+          await Promise.all(thumbnails);
+
+        }
+    
+        const uploadedFiles = await uploadService.upload(enhancedFiles, config);
+    
+        // Send 200 `ok`
+        ctx.send(uploadedFiles.map(f => ({ ...f, thumbnail: `/uploads/${f.name}.png` })));
+    },
+    async destroyFile(ctx) {
+      const { id } = ctx.params;
+      const config = await strapi
+        .store({
+          environment: strapi.config.environment,
+          type: 'plugin',
+          name: 'upload',
+        })
+        .get({ key: 'provider' });
+  
+      const file = await strapi.plugins['upload'].services.upload.fetch({ id });
+  
+      if (!file) {
+        return ctx.notFound('file.notFound');
+      }
+  
+      await strapi.plugins['upload'].services.upload.remove(file, config);
+
+      const thumbnailRoute = `${strapi.config.public.path}/uploads/${file.name.split('.')[0]}.png`;
+      if (fs.existsSync(thumbnailRoute)) {
+        fs.unlinkSync(thumbnailRoute)
+      }
+
+      ctx.send(file);
     }
 };
---
+++ extensions/users-permissions/controllers/User.js
@@ -0,0 +1,26 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+const sanitizeUser = user =>
+  sanitizeEntity(user, {
+    model: strapi.query('user', 'users-permissions').model,
+  });
+
+module.exports = {
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        let data = await strapi.plugins['users-permissions'].services.user.fetch({
+          id,
+        });
+    
+        if (data) {
+          data = sanitizeUser(data);
+        }
+
+        data.videos = data.videos
+            .filter(v => v.hasOwnProperty('id'))
+            .map(v => ({ ...v, thumbnail: `/uploads/${v.name.split('.')[0]}.png`}))
+
+        // Send 200 `ok`
+        ctx.send(data);
+    }
+}
